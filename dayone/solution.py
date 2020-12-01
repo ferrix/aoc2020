b86bf7e0992a582a3758eae1e1e93ce817d89a90
Ferrix Hovi <ferrix@ferrix.fi>
2020-12-01 17:14:12 +0200

    Day 1, Puzzle 2

--- dayone/solution.py
+++ dayone/solution.py
@@ -9,3 +9,17 @@ with open('input.txt') as f:
             print(value * expenses[value])
             break
         expenses[2020 - value] = value
+
+del expenses
+
+with open('input.txt') as f:
+    lines = [int(line) for line in f]
+
+expenses = {}
+
+for i in range(0, len(lines)-1):
+    for j in range(i+1, len(lines)):
+        if lines[j] in expenses:
+            print(lines[j] * expenses[lines[j]][0] * expenses[lines[j]][1])
+            sys.exit(0)
+        expenses[2020 - lines[i] - lines[j]] = (lines[i], lines[j])
