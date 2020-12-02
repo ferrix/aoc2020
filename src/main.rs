2a8ac1309ac91d763b9c69fe3b68c10f9b4aef88
Ferrix Hovi <ferrix@ferrix.fi>
2020-12-02 21:09:38 +0200

    Cleaned main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 extern crate aoc_runner;
-use aoc_runner_derive::aoc_main;
-
-//#[macro_use]
 extern crate aoc_runner_derive;
-
-//pub mod day1;
-//pub mod day2;
-//aoc_main! { year = 2020 }
-
 extern crate advent_of_code_2020;
+
+use aoc_runner_derive::aoc_main;
 
 aoc_main! { lib = advent_of_code_2020 }
